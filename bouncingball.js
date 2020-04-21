8c124685567d925682279303405b6cb6f47e627c
Giwan Park <aaamy91@naver.com>
2020-04-21 23:15:36 +0100

    Update bouncingball.js
    Changed random color method
    add ball moving function

--- bouncingball.js
+++ bouncingball.js
@@ -2,21 +2,25 @@ const canvas = document.querySelector("#canvas");
 const c = canvas.getContext("2d");
 const tx = window.innerWidth;
 const ty = window.innerHeight;
+var grav = 0.99;
 canvas.height = ty;
 canvas.width = tx;
 
 var colour = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
-function randomColour()
-{
-    return "#" + 
-    colour[Math.floor(Math.random() * 100) % 15] + 
-    colour[Math.floor(Math.random() * 100) % 15] + 
-    colour[Math.floor(Math.random() * 100) % 15] + 
-    colour[Math.floor(Math.random() * 100) % 15] + 
-    colour[Math.floor(Math.random() * 100) % 15] + 
-    colour[Math.floor(Math.random() * 100) % 15];
-}
+function randomColour() {
+    return (
+      "rgba(" +
+      Math.round(Math.random() * 250) +
+      "," +
+      Math.round(Math.random() * 250) +
+      "," +
+      Math.round(Math.random() * 250) +
+      "," +
+      Math.ceil(Math.random() * 10) / 10 +
+      ")"
+    );
+  }
 
 function Ball()
 {
@@ -24,6 +28,9 @@ function Ball()
     this.radius = Math.random() * 20 + 14;
     this.x = Math.random() * (tx - this.radius * 2) + this.radius;
     this.y = Math.random() * (ty - this.radius);
+    this.dy = Math.random() * 2;
+    this.dx = Math.round((Math.random() - 0.5) * 10);
+    this.vel = Math.random() /5;
 
     this.update = function() {
         c.beginPath();
@@ -39,12 +46,34 @@ for (var i=0; i<50; i++){
     bal.push(new Ball());
 }
 
+
 function drawingBalls()
 {
     requestAnimationFrame(drawingBalls);
     c.clearRect(0,0,tx,ty);
     for(i=0; i<50; ++i)
+    {
         bal[i].update();
+        bal[i].y += bal[i].dy;
+        bal[i].x += bal[i].dx;
+
+        if (bal[i].y + bal[i].radius >= ty)
+        {
+            bal[i].dy = -bal[i].dy * grav;
+        }
+        else
+        {
+            bal[i].dy += bal[i].vel;
+        }    
+        if(bal[i].x + bal[i].radius > tx)
+        {
+            bal[i].x = bal[i].radius;
+        }
+        else if(bal[i].x - bal[i].radius < 0)
+        {
+            bal[i].x = tx - bal[i].radius;
+        }
+    }
 }
 
 drawingBalls();
